f7e53e0bd30210463e54a1659e0e9732fe25b04a
AleksandrBausher <bausheraleksandr@gmail.com>
2023-05-30 23:01:21 -0400

    added user controller created routes created server.js created API in alient created auth in client created user-routes created api and auth in server

--- client/src/components/utils/SignupForm.js
+++ client/src/components/utils/SignupForm.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
-import './SignupForm.css'
+import React, { useState } from "react";
+import "./SignupForm.css";
+
+import { createUser } from '../../utils/API';
+
+import Auth from "../../utils/auth.js"
+
+
 const SignupForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -17,38 +23,64 @@ const SignupForm = () => {
     setConfirmPassword(e.target.value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // signup logic here
-    console.log('Signup form submitted');
-    console.log('Email:', email);
-    console.log('Password:', password);
-    console.log('Confirm Password:', confirmPassword);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    try {
+      const response = await createUser({ email: email, password: password });
+      console.log("this is the response",response)
+      if (!response.ok) {
+        throw new Error("something went wrong!");
+      }
+
+      const { token, user } = await response.json();
+      console.log(user);
+      Auth.login(token);
+    } catch (err) {
+      console.error(err);
+    }
+
+    setEmail("")
+    setPassword("")
+    setConfirmPassword("")
   };
 
   return (
-    <div className='signupContainer'>
-    <form className='signupForm' onSubmit={handleSubmit}>
-      
-      <p>Create Accout</p>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={handleEmailChange} />
-      </label>
-      
-      <label>
-        Password:
-        <input type="password" value={password} onChange={handlePasswordChange} />
-      </label>
-      
-      <label>
-        Confirm Password:
-        <input type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} />
-      </label>
-      
-      <button type="submit">Sign Up</button>
-      
-    </form>
+    <div className="signupContainer">
+      <form className="signupForm" onSubmit={handleSubmit}>
+        <p>Create Accout</p>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} />
+        </label>
+
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </label>
+
+        <label>
+          Confirm Password:
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+          />
+        </label>
+
+        <button type="submit">Sign Up</button>
+      </form>
     </div>
   );
 };
