9a692d5af76f982d9c98352c0b5135002772e2aa
Terry <misterfreemann@gmail.com>
2023-06-02 15:37:08 -0400

    styled nav links to tabs

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import Nav from "../Nav/Nav";
 import logoImg from "../images/tots2treasures.png"
+import Cart from "../Cart/Cart";
 const Header = () => {
+  const [show, setShow] = useState(false)
   return (
+
     <header>
       <div className="header">
-        <h1><img className="t2tLogo" src={logoImg} alt="logo"></img></h1>
-        
+        <div className="tabContainer">
+      <img className="t2tLogo" src={logoImg} alt="logo"></img>
+     
+  
+        <Nav />
+      </div>
 
         <ul className="authButtons">
-        <Nav />
+        <li>
+          <a onClick={()=>{setShow(true)}} className="cartLogo navLink">
+            <i class="fa-solid fa-cart-shopping fa-xl fa-10x "></i>
+          </a>
+        </li>
+        <Cart onClose={() => setShow(false)} show={show} />
           <li>
             <a href='/login'className="loginButton">Login</a>
           </li>
