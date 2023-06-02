a95722e2585a6ad12ab3d2c4232f629218b747ed
AleksandrBausher <bausheraleksandr@gmail.com>
2023-06-02 10:09:05 -0400

    creating final pag

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Cart from "./components/Cart/Cart";
 import Inventory from "./components/Inventory/Inventory";
 import Purchases from "./components/Purchases/Purchases";
 import "./App.css";
+import Final from "./components/Final/Final";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="/signup" element={<SignupForm />} />
         <Route path="/login" element={<LoginForm />} />
+        <Route path="/final" element={<Final/>} />
       </Routes>
       <Footer />
     </div>
---
+++ client/src/components/Final/Final.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Final.css"
+
+const Final = () => {
+  return (
+    <div className="finalScreen">
+      <i className="fa-solid fa-badge-check"></i>
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default Final;
