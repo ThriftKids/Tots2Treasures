6bdeaf0ced50d0d6e8f3f06c87d2e5fb473b72b7
Terry <misterfreemann@gmail.com>
2023-06-03 16:43:53 -0400

    restyled shopping cart, added pop up effect to shopping cart

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Cart.css";
 import CartCard from "./Card";
 
@@ -9,9 +9,10 @@ const Cart = (props) => {
   if (!props.show) {
     return null;
   }
+
   return (
-    <div className="modal" onClick={props.onClose}>
-      <div className="cartContent" onClick={(e) => e.stopPropagation()}>
+    <div className={`modal`} onClick={props.onClose}>
+      <div className={`cartContent`} onClick={(e) => e.stopPropagation()}>
         <div className="closeCartButtonContainer">
         <button onClick={props.onClose} className="closeCartButton">
         <i class="fa-solid fa-square-xmark fa-xl"></i>
@@ -20,14 +21,19 @@ const Cart = (props) => {
         <h2 className="shoppingCartTitle">Your Shopping Cart</h2>
 
         <div className="column cardContainer">
-          <CartCard title="Title 1" price="$100" img={one} />
-          <CartCard title="Title 1" price="$100" img={testImage} />
-          <CartCard title="Title 1" price="$100" img={one} />
+          <CartCard title="Title 1" desc='some description' price="$100" img={one} />
+          <CartCard title="Title 1" desc='some description' price="$100" img={testImage} />
+          <CartCard title="Title 1" desc='some description' price="$100" img={one} />
+          <CartCard title="Title 1" desc='some description' price="$100" img={one} />
+          <CartCard title="Title 1" desc='some description' price="$100" img={one} />
+          <CartCard title="Title 1" desc='some description' price="$100" img={one} />
+          <CartCard title="Title 1" desc='some description' price="$100" img={one} />
+          <CartCard title="Title 1" desc='some description' price="$100" img={one} />
+        </div>
           <div className="cartFooterContainer">
             <p>Total: $300</p>
             <button className="purchaseCartButton">Purchase</button>
           </div>
-        </div>
       </div>
     </div>
   );
