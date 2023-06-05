6b7c5fc0b11f00d8540d59593eba9f98160f2c53
Jacob MacDonald <100619060+MadDonald@users.noreply.github.com>
2023-06-05 19:15:24 -0400

    Delete server/models directory

--- server/models/Index.js
+++
@@ -1,6 +0,0 @@
-const User = require('./User');
-const Product = require('./Product');
-const Tag = require('./Tag');
-const Post = require('./Post');
-
-module.exports = { User, Product, Tag, Post };
