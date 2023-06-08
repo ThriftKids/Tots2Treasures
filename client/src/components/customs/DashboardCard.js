a90a9eaa46be25071dde54a7e6b80f670b44fe86
Terry <misterfreemann@gmail.com>
2023-06-07 20:34:46 -0400

    added notifications 'item added to cart/item is already in your cart'

--- client/src/components/customs/DashboardCard.js
+++ client/src/components/customs/DashboardCard.js
@@ -3,26 +3,22 @@ import DashboardModal from "../Dashboard/DashboardModal";
 import { useStoreContext } from "../../utils/GlobalState";
 
 import { ADD_TO_CART } from "../../utils/actions";
-import { useQuery } from '@apollo/client';
+import { useQuery } from "@apollo/client";
 import { idbPromise } from "../../utils/helpers";
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { QUERY_PRODUCTS } from "../../utils/queries";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const DashboardCard = (props) => {
   const [show, setShow] = useState(false);
   const [state, dispatch] = useStoreContext();
-//function to add card to cart , but hardcoded cards only
-  const handleAddToCart= ()=>{
-   state.cart.push({...props})
-  }
 
   const handleViewMore = () => {
     setShow(true);
-
   };
 
-  const [state, dispatch] = useStoreContext();
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
@@ -32,10 +28,10 @@ const DashboardCard = (props) => {
         products: data.products,
       });
       data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
+        idbPromise("products", "put", product);
       });
     } else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
+      idbPromise("products", "get").then((products) => {
         dispatch({
           type: UPDATE_PRODUCTS,
           products: products,
@@ -44,29 +40,39 @@ const DashboardCard = (props) => {
     }
   }, [data, loading, dispatch]);
 
-  const {
-    img,
-    title,
-    _id,
-    price,
-    desc
-  } = props;
+  const { img, title, _id, price, desc } = props;
 
-  const { cart } = state
+  const { cart } = state;
 
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
-    if (!itemInCart && props && props._id) {
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
+    if (itemInCart) {
+      toast.error("This item is already in your cart", {className:'error-toast',bodyClassName: 'error-toast-body',progressClassName: 'error-toast-progress'});
+    } else if (props && props._id) {
       dispatch({
         type: ADD_TO_CART,
-        product: { ...props }
+        product: { ...props },
       });
-      idbPromise('cart', 'put', {...props} );
+      idbPromise("cart", "put", { ...props });
+      toast.success("Item added to your cart" ,{
+        className: 'success-toast',bodyClassName: 'success-toast-body',progressClassName: 'success-toast-progress'
+      });
     }
-  }
+  };
 
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <div className="dashboardCard">
         <div className="img">
           <img src={img} alt={props.tag.split(" ")[0]} />
@@ -78,22 +84,24 @@ const DashboardCard = (props) => {
 
         <div className="row bottomRow">
           <p>{desc}</p>
-
         </div>
 
         <div className="rowForButtons">
-          
-        <div className="viewMoreDashboard" onClick={handleViewMore}>
-          View more
+          <div className="viewMoreDashboard" onClick={handleViewMore}>
+            View more
+          </div>
+
+          <button className="addToCartButton" onClick={addToCart}>
+            + Add To Cart
+          </button>
         </div>
-
-
-          <button className="addToCartButton" onClick={addToCart}>+ Add To Cart</button>
-
-        </div>
-
       </div>
-      <DashboardModal tags={['Toys','Kitchen','Clothing','Outdoor','Electronics']} show={show} onClose={()=>setShow(false)} {...props}/>
+      <DashboardModal
+        tags={["Toys", "Kitchen", "Clothing", "Outdoor", "Electronics"]}
+        show={show}
+        onClose={() => setShow(false)}
+        {...props}
+      />
     </>
   );
 };
